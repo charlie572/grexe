a78d55868a74f980df6086f3b925c74e47bf7a93
Charlie Bareham <barehamcharlie@gmail.com>
2025-12-31 13:04:17 +0000

    Select commits

--- src/git_rebase_extended/main.py
+++ src/git_rebase_extended/main.py
@@ -1,6 +1,6 @@
 import argparse
 from os import path
-from typing import List
+from typing import List, Optional
 
 from git import Repo, Commit
 from textual.app import App
@@ -22,9 +22,43 @@ class FileChange(Widget):
 class GitRebaseExtendedApp(App):
     CSS_PATH = "main.tcss"
 
+    BINDINGS = [
+        ("j", "move_down", "Move the cursor down."),
+        ("k", "move_up", "Move the cursor up."),
+        ("v", "select", "Select commit."),
+    ]
+
     def __init__(self, commits: List[Commit], *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._commits: List[Commit] = commits
+        self._commit_widgets: Optional[List[List[Widget]]] = None
+        self._active_index = None
+
+    def _set_active(self, index):
+        # clear previous active commit
+        for widgets in self._commit_widgets:
+            for widget in widgets:
+                widget.remove_class("active")
+
+        # set background of new active commit
+        widgets = self._commit_widgets[index]
+        for widget in widgets:
+            widget.add_class("active")
+
+        self._active_index = index
+
+    def action_move_up(self):
+        new_index = max(0, self._active_index - 1)
+        self._set_active(new_index)
+
+    def action_move_down(self):
+        new_index = min(len(self._commits) - 1, self._active_index + 1)
+        self._set_active(new_index)
+
+    def action_select(self):
+        widgets = self._commit_widgets[self._active_index]
+        for widget in widgets:
+            widget.toggle_class("selected")
 
     def compose(self):
         files = sum(
@@ -48,15 +82,25 @@ class GitRebaseExtendedApp(App):
                 yield Label(filename, classes="filename")
 
             # commit rows
+            self._commit_widgets = []
             for commit in self._commits:
-                yield Label("pick", classes="rebase_action")
-                yield Label(commit.hexsha[:7], classes="hexsha")
+                widgets = [
+                    Label("pick", classes="rebase_action"),
+                    Label(commit.hexsha[:7], classes="hexsha"),
+                ]
 
                 first_message_line = commit.message.split("\n")[0]
-                yield Label(first_message_line, classes="commit_message")
+                message_label = Label(first_message_line, classes="commit_message")
+                widgets.append(message_label)
 
                 for file in files:
-                    yield FileChange(file in commit.stats.files)
+                    widgets.append(FileChange(file in commit.stats.files))
+
+                self._commit_widgets.append(widgets)
+                yield from widgets
+
+    def on_mount(self):
+        self._set_active(0)
 
 
 def main():
