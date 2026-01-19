f2a362663875b9e490ef1c5b55d54244e63bf034
Charlie Bareham <barehamcharlie@gmail.com>
2026-01-19 21:07:10 +0000

    Store "active" variable in each node

--- src/git_rebase_extended/file_selector.py
+++ src/git_rebase_extended/file_selector.py
@@ -1,17 +1,26 @@
 import os.path
 from os import PathLike
-from typing import Iterable
+from typing import Iterable, Dict
+
+from rich.style import Style
+from rich.text import Text
 
 from textual.widgets import Tree
+from textual.widgets._tree import TreeNode, TreeDataType
 
 
 class FileSelector(Tree):
     def __init__(self, file_paths: Iterable[str | PathLike], *args, **kwargs):
         common_path = os.path.commonpath(file_paths)
-        super().__init__(common_path, *args, **kwargs)
+        super().__init__(
+            common_path,
+            data={"path": common_path, "active": True},
+            *args,
+            **kwargs,
+        )
 
         file_paths = [os.path.relpath(path, common_path) for path in file_paths]
-        nodes = {"": self.root}
+        nodes: Dict[str : TreeNode[str]] = {"": self.root}
         self.root.expand()
         for path in file_paths:
             path_elements = path.split(os.path.sep)
@@ -21,4 +30,30 @@ class FileSelector(Tree):
                     parent_node_path = os.path.sep.join(path_elements[:i])
                     parent_node = nodes[parent_node_path]
                     node_name = path_elements[i]
-                    nodes[node_path] = parent_node.add(node_name, expand=True)
+                    nodes[node_path] = parent_node.add(
+                        node_name,
+                        expand=True,
+                        data={
+                            "path": path,
+                            "active": True,
+                        },
+                    )
+
+    def on_tree_node_selected(self, event):
+        node: TreeNode[str] = event.node
+        active = not node.data["active"]
+        self.set_nodes_active(node, active)
+
+    @classmethod
+    def set_nodes_active(cls, node: TreeNode[str], active: bool):
+        node.data["active"] = active
+        for child in node.children:
+            cls.set_nodes_active(child, active)
+
+    def render_label(
+        self, node: TreeNode[TreeDataType], base_style: Style, style: Style
+    ) -> Text:
+        text = node.label
+        if node.data["active"]:
+            text += " *"
+        return Text.assemble(text, style=Style.chain(base_style, style))
